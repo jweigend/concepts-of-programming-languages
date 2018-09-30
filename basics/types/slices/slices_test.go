b736e123e3783dfabb35f2872c44f633c6bec51f
jweigend <johannes.weigend@qaware.de>
2018-09-30 16:46:09 +0200

    Slices

---
+++ basics/types/slices/slices_test.go
@@ -0,0 +1,19 @@
+package slices
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSliceAppend(t *testing.T) {
+	array := [2]string{"A", "B"}
+	slice := array[:] // slice the array
+	fmt.Printf("Slice: %v, Capacity: %v, Length: %v\n", slice, cap(slice), len(slice))
+
+	// append() does not work for arrays, only for slices.
+	slice = append(slice, "C")
+	fmt.Printf("Slice: %v, Capacity: %v, Length: %v\n", slice, cap(slice), len(slice))
+	slice = append(slice, "D")
+	slice = append(slice, "E")
+	fmt.Printf("Slice: %v, Capacity: %v, Length: %v\n", slice, cap(slice), len(slice))
+}
