fc0ddc565da947852b54df502d6e92e0047c6031
jweigend <johannes.weigend@qaware.de>
2018-10-28 11:25:48 +0100

    Lambda Calculus

--- fp/lambdacalculus/main.go
+++ fp/lambdacalculus/main.go
@@ -13,17 +13,12 @@ func main() {
 	// Lambda Calculus with JS
 	// TRUE = x => y => x;
 	// FALSE = x => y => y;
-	// NOT = f => x => y => f(y)(x);
+	// NOT = b => b(FALSE)(TRUE);
 
-	// Some sample functions - strings are only used for debugging purposes
+	// This is the key: A Recursive function definition for all functions!!!
+	type fnf func(fnf) fnf
 
-	type fnf func(fnf) fnf // recursive function definition
-
-	f := func(x fnf) fnf { fmt.Printf("f()=%v\n", x); return x }
-
-	g := func(y fnf) fnf { fmt.Printf("g()=%v\n", y); return y }
-
-	id := func(z fnf) fnf { return z } // print
+	ID := func(x fnf) fnf { return x }
 
 	// TRUE as function: λx.λy.x
 	True := func(x fnf) fnf {
@@ -31,34 +26,40 @@ func main() {
 			return x
 		}
 	}
+
 	// FALSE as function: λx.λy.y
 	False := func(x fnf) fnf {
 		return func(y fnf) fnf {
 			return y
 		}
 	}
-	// FALSE as function: λx.λy.y
+
+	// NOT as function: λb.b false true
 	Not := func(b fnf) fnf {
 		return b(False)(True)
 	}
 
-	fmt.Printf("Id = %p\n", id)
+	fmt.Printf("Id = %p\n", ID)
 	fmt.Printf("True = %p\n", True)
 	fmt.Printf("False = %p\n", False)
 
-	// false
+	// should print false
 	fmt.Printf("True(False)(True) = %p\n", True(False)(True))
 	fmt.Printf("Not(True) = %p\n", Not(True))
 
-	// true
+	// should print true
 	fmt.Printf("False(False)(True) = %p\n", False(False)(True))
 	fmt.Printf("Not(False) = %p\n", Not(False))
 
-	// select f
-	False(False)(True)(f)(g)(id)
-	Not(False)(f)(g)(id)
+	// debugging functions
+	f := func(x fnf) fnf { fmt.Printf("f()\n"); return x }
+	g := func(y fnf) fnf { fmt.Printf("g()\n"); return y }
 
-	// select g
-	True(False)(True)(f)(g)(id)
-	Not(True)(f)(g)(id)
+	// select and call first function f(ID)
+	False(False)(True)(f)(g)(ID)
+	Not(False)(f)(g)(ID)
+
+	// select and call second function g(ID)
+	True(False)(True)(f)(g)(ID)
+	Not(True)(f)(g)(ID)
 }
