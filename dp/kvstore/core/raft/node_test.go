e4a1479ae72e0f319b613ec60762ed75fed904a2
jweigend <johannes.weigend@qaware.de>
2018-11-29 20:26:28 +0100

    Testing.

--- dp/kvstore/core/raft/node_test.go
+++ dp/kvstore/core/raft/node_test.go
@@ -9,22 +9,6 @@ import (
 	"time"
 )
 
-func TestHeartbeat(t *testing.T) {
-	// single node cluster
-	n1 := NewNode(0)
-	nodes := []*Node{n1}
-	n1.cluster = NewCluster(nodes)
-
-	// startHeartbeat is only allowed in leader state
-	n1.statemachine.Next(CANDIDATE)
-	n1.statemachine.Next(LEADER)
-	n1.heartbeatTimer.resetC <- true
-
-	// wait two second --> check console output
-	time.Sleep(2000 * time.Millisecond)
-	n1.Stop()
-}
-
 func TestElection(t *testing.T) {
 
 	n1 := NewNode(0)
@@ -37,7 +21,7 @@ func TestElection(t *testing.T) {
 
 	cluster.StartAll()
 
-	time.Sleep(5000 * time.Millisecond)
+	time.Sleep(4000 * time.Millisecond)
 
 	ok, err := cluster.Check()
 	if !ok {
@@ -63,7 +47,7 @@ func TestFailover(t *testing.T) {
 
 	cluster.StopLeader()
 
-	time.Sleep(10000 * time.Millisecond)
+	time.Sleep(5000 * time.Millisecond)
 
 	ok, err := cluster.Check()
 	if !ok {
@@ -84,7 +68,7 @@ func TestFailoverResume(t *testing.T) {
 	cluster.StartAll()
 	defer cluster.StopAll()
 
-	time.Sleep(8000 * time.Millisecond)
+	time.Sleep(5000 * time.Millisecond)
 
 	// stop leader
 	ns := cluster.StopLeader()
@@ -100,29 +84,4 @@ func TestFailoverResume(t *testing.T) {
 	if !ok {
 		t.Error(err)
 	}
-}
-
-func TestBigCluster(t *testing.T) {
-
-	n1 := NewNode(0)
-	n2 := NewNode(1)
-	n3 := NewNode(2)
-	n4 := NewNode(3)
-	n5 := NewNode(4)
-
-	nodes := []*Node{n1, n2, n3, n4, n5}
-
-	cluster := NewCluster(nodes)
-
-	cluster.StartAll()
-
-	time.Sleep(10000 * time.Millisecond)
-
-	ok, err := cluster.Check()
-	if !ok {
-		t.Error(err)
-	}
-
-	cluster.StopAll()
-	time.Sleep(1000 * time.Millisecond) // wait for grace shutdown
 }
