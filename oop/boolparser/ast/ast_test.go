5ba6affb146c15937641a0a2654baad138b44379
jweigend <johannes.weigend@qaware.de>
2019-11-06 09:59:41 +0100

    AST

--- oop/boolparser/ast/ast_test.go
+++ oop/boolparser/ast/ast_test.go
@@ -8,26 +8,28 @@ import (
 
 func TestAST(t *testing.T) {
 
-	// A AND B OR C
-	ast := Or{And{Val{"A"}, Val{"B"}}, Val{"C"}}
+	// Expression: "A AND B OR !C"
+	ast := Or{And{Val{"A"}, Val{"B"}}, Not{Val{"C"}}}
 
+	// Table to test all combinations for A, B, C -> 2^3 = 8 combinations.
+	// Format of Table { Val(A), Val(B), Val(C), RESULT }
 	truthTable := [][]bool{
-		{false, false, false, false},
-		{false, false, true, true},
-		{false, true, true, true},
-		{false, true, false, false},
-		{true, false, false, false},
+		{false, false, false, true},
+		{false, false, true, false},
+		{false, true, true, false},
+		{false, true, false, true},
+		{true, false, false, true},
 		{true, true, false, true},
-		{true, false, true, true},
+		{true, false, true, false},
 		{true, true, true, true},
 	}
 
-	for i, tt := range truthTable {
-		
+	for _, tt := range truthTable {
+
 		vars := map[string]bool{"A": tt[0], "B": tt[1], "C": tt[2]}
+		expected := tt[3]
 		result := ast.Eval(vars)
-		
-		expected := truthTable[i][3]
+
 		if result != expected {
 			t.Errorf("Expected %v but got %v. (Expression := %v, Vars := %v)", expected, result, ast, vars)
 		}
